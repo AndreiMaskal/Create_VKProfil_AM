17ab48c8216f68ce88d0eb44a6c1a20e4b29cd0b
Andrik <masmay.pl@gmail.com>
2022-05-18 21:35:24 +0200

    initial commit

---
+++ Create_VKProfil_AM/Create_VKProfil_AM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Create_VKProfil_AM
+//
+//  Created by Andrei Maskal on 18/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
